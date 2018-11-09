d3cf39cb692bbfc8b6f00286de27e8e3b9227b63
Rodrigo Alvarado <alv16106@uvg.edu.gt>
2018-11-09 11:56:42 -0600

    Object level permissions (?)

---
+++ nota/permissions.py
@@ -0,0 +1,18 @@
+from django.db.models import Q
+
+from . import models
+
+from rest_framework import permissions
+
+class IsOwnerOrShared(permissions.BasePermission):
+  def can_access_note(self, request, view, obj):
+    return models.Shared.objects.filter(
+      Q(note__owner=request.user, note=obj) |
+      Q(shared_to=request.user, note=obj)
+    ).exist()
+
+    
+    
+    """     if models.Shared.objects.filter(shared_to=request.user, note=obj):
+      return True
+    return obj.owner == request.user """
--- nota/views.py
+++ nota/views.py
@@ -1,11 +1,12 @@
 from rest_framework import viewsets
-
+from nota.permissions import IsOwnerOrShared
 from . import models, serializers
 
 # Create your views here.
 class NoteModelViewSet(viewsets.ModelViewSet):
     queryset = models.Note.objects.all()
     serializer_class = serializers.NoteSerializer
+    permission_classes = (IsOwnerOrShared)
 
 class FriendshipModelViewSet(viewsets.ModelViewSet):
     queryset = models.Friendship.objects.all()
