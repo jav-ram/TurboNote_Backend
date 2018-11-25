48b0e5acb47f4a5d58e4a1d834e952366877f77c
ram16230 <ram16230@uvg.edu.gt>
2018-11-25 15:31:19 -0600

    ADD get all friends of an user

--- cuaderno/views.py
+++ cuaderno/views.py
@@ -3,14 +3,16 @@ from django.contrib.auth.models import User
 from rest_framework.permissions import BasePermission, IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django.db.models import Q
 
 from cuaderno.permissions import IsOwnerOrReadOnly
 from . import models, serializers
 from nota import models as modelNota, serializers as NotaSerializer
 # Create your views here.
 
+
 class NotebookModelViewSet(viewsets.ModelViewSet):
-    # /notebook/ devuelve todo 
+    # /notebook/ devuelve todo
     permission_classes = (IsOwnerOrReadOnly, )
     queryset = models.Notebook.objects.all()
     serializer_class = serializers.NotebookSerializer
@@ -19,18 +21,20 @@ class NotebookModelViewSet(viewsets.ModelViewSet):
     # /notebook/all-notes/ devuelve todas las NOTAS dentro de del cuaderno
     def all_notes(self, request, pk=None):
         notebook = self.get_object()
-        notes = modelNota.Note.objects.filter(owner = notebook.pk)
+        notes = modelNota.Note.objects.filter(owner=notebook.pk)
         notes_serialize = NotaSerializer.NoteSerializer(notes, many=True)
 
         return Response(
             notes_serialize.data
         )
 
+
 class UserPermissions(BasePermission):
     def has_permission(self, request, view):
         if view.action == 'create':
             return True
         return request.user and request.user.is_authenticated
+
 
 class UserModelViewSet(viewsets.ModelViewSet):
     permission_classes = (UserPermissions, )
@@ -41,8 +45,9 @@ class UserModelViewSet(viewsets.ModelViewSet):
     # /user/all-notebooks/ devuelve todos los CUADERNOS del usuario
     def all_notebooks(self, request, pk=None):
         usuario = self.get_object()
-        notebooks = models.Notebook.objects.filter(owner = usuario.pk)
-        notebooks_serialize = serializers.NotebookSerializer(notebooks, many= True)
+        notebooks = models.Notebook.objects.filter(owner=usuario.pk)
+        notebooks_serialize = serializers.NotebookSerializer(
+            notebooks, many=True)
 
         return Response(
             notebooks_serialize.data
@@ -52,11 +57,34 @@ class UserModelViewSet(viewsets.ModelViewSet):
     # /user/all-notes/ devuelve todos las NOTAS del usuario
     def all_notes(self, request, pk=None):
         usuario = self.get_object()
-        notes =  modelNota.Note.objects.filter(owner__owner=usuario)
-        notes_serialize = NotaSerializer.NoteSerializer(notes, many= True)
+        notes = modelNota.Note.objects.filter(owner__owner=usuario)
+        notes_serialize = NotaSerializer.NoteSerializer(notes, many=True)
 
         return Response(
             notes_serialize.data
+        )
+
+    @action(methods=['GET'], detail=True, url_path='friends')
+    # /user/friends/ devuelve todos las AMIGOS del usuario
+    def all_notes(self, request, pk=None):
+        user = self.get_object()
+        friendships = modelNota.Friendship.objects.filter(
+            Q (friend1=user.pk) |
+            Q (friend2=user.pk)
+        )
+        friends = []
+        for friendship in friendships:
+            friend1 = friendship.friend1
+            friend2 = friendship.friend2
+            if friend1 != user:
+                friends.append(friend1)
+            elif friend2 != user:  
+                friends.append(friend2)
+
+        friends_serialize = serializers.UserSerializer(friends, many=True)
+
+        return Response(
+            friends_serialize.data
         )
 
     @action(methods=['GET'], detail=True, url_path='all-notes-from')
@@ -70,7 +98,7 @@ class UserModelViewSet(viewsets.ModelViewSet):
 
         # verificar la amistad falta
         if (modelNota.Friendship.objects.filter(friend1=usuario, friend2=friend).count() > 0) or (modelNota.Friendship.objects.filter(friend1=friend, friend2=usuario).count() > 0):
-            # son amigos 
+            # son amigos
             # get todos las notas
             shares = modelNota.Shared.objects.filter(owner=friend)
             notes = []
@@ -80,14 +108,10 @@ class UserModelViewSet(viewsets.ModelViewSet):
                 notes.append(modelNota.Note.objects.get(pk=pk_id))
 
             notes_serialize = NotaSerializer.NoteSerializer(notes, many=True)
-            return Response( notes_serialize.data )
+            return Response(notes_serialize.data)
         else:
-            return Response( {'error': "no son amigos"} )
-
+            return Response({'error': "no son amigos"})
 
         return Response(
-            {"usuario" : usuario_serialize.data, "amigo":  friend_serializer.data}
+            {"usuario": usuario_serialize.data, "amigo":  friend_serializer.data}
         )
-    
-        
-        
--- nota/permissions.py
+++ nota/permissions.py
@@ -4,16 +4,13 @@ from . import models
 
 from rest_framework import permissions
 
-class IsOwnerOrShared(permissions.BasePermission):
-  def can_access_note(self, request, view, obj):
-    return models.Shared.objects.filter(
-      Q(note__owner__owner=request.user, note=obj) |
-      Q(shared_to=request.user, note=obj) |
-      Q(owner=request.user, note=obj)
-    ).exist()
 
-    
-    
-    """     if models.Shared.objects.filter(shared_to=request.user, note=obj):
-      return True
-    return obj.owner == request.user """
+class IsOwnerOrShared(permissions.BasePermission):
+    def can_access_note(self, request, view, obj):
+        return models.Shared.objects.filter(
+            Q(note__owner__owner=request.user, note=obj) |
+            Q(shared_to=request.user, note=obj) |
+            Q(owner=request.user, note=obj)
+        ).exist()
+
+
